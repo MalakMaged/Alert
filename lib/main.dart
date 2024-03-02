8078a0f6762dc39fa2af038ee408d59e908ed763
Malak Maged <101951317+MalakMaged@users.noreply.github.com>
2024-03-02 23:07:46 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,101 @@
-import 'dart:convert';
-import 'package:crimebott/DefaultFirebaseOptions.dart';
-import 'package:crimebott/firebase_options.dart';
-import 'package:crimebott/login_page.dart';
-import 'package:flutter/services.dart';
-import 'auth_page.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  //   DeviceOrientation.portraitDown,
-  // ]);
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    
-  } catch (e) {
-    print('Error initializing Firebase: $e');
-  }
-
+//key : AIzaSyB5GDGXGS7IHSwJI95-Y1OMcEfmTsSuiN
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      title: 'Flutter Google Maps Polyline',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late GoogleMapController _mapController;
+  late LocationData _currentLocation;
+  late LocationData _destinationLocation;
+  Set<Marker> _markers = {};
+  Set<Polyline> _polylines = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+    _destinationLocation = LocationData.fromMap({
+      "latitude": 31.2001,
+      "longitude": 29.9187,
+    });
+  }
+
+  void _getCurrentLocation() async {
+    var location = Location();
+    _currentLocation = await location.getLocation();
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId("currentLocation"),
+        position:
+            LatLng(_currentLocation.latitude!, _currentLocation.longitude!),
+        infoWindow: InfoWindow(title: "Current Location"),
+      ));
+    });
+  }
+
+  void _drawPolyline() {
+    setState(() {
+      _polylines.add(Polyline(
+        polylineId: PolylineId("polyline"),
+        color: Colors.blue,
+        width: 5,
+        points: [
+          LatLng(_currentLocation.latitude!, _currentLocation.longitude!),
+          LatLng(
+              _destinationLocation.latitude!, _destinationLocation.longitude!),
+        ],
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Google Maps"),
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(24.8960309, 67.0792159),
+              zoom: 12,
+            ),
+            onMapCreated: (controller) {
+              _mapController = controller;
+            },
+            markers: _markers,
+            polylines: _polylines,
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: ElevatedButton(
+              onPressed: _drawPolyline,
+              child: Text("Draw Polyline"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
