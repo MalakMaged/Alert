bc008ebcd25a9e603250ea5992ee088834132c6c
FouadAhmed6 <156611941+FouadAhmed6@users.noreply.github.com>
2024-05-04 16:23:51 +0300

    map(alternative routes and safest route)

--- lib/map.dart
+++ lib/map.dart
@@ -1,347 +1,208 @@
-import 'dart:typed_data';
-import 'constants.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:location/location.dart';
-import 'package:intl/intl.dart';
-
-
+import 'package:http/http.dart' as http;
+import 'secrets.dart'; // Ensure your API key is safely stored here
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
 
   @override
-  _MapPageState createState() => _MapPageState();
+  State<MapPage> createState() => _MapPageState();
 }
 
-
 class _MapPageState extends State<MapPage> {
-  late GoogleMapController mapController;
-  LocationData? currentLocation;
-  Set<Marker> crimeMarkers = {};
-  bool placingPin = false;
+  final LatLng _initialPosition = const LatLng(30.0285952, 31.5523072);
+  LatLng? _startLocation;
+  LatLng? _destinationLocation;
+  Set<Polyline> _polylines = {};
+  Set<LatLng> _avoidCoordinates = {LatLng(30.027439045271805, 31.528331641070277)}; // Set to store avoid coordinates
 
   @override
-  void initState() {
-    
-    super.initState();
-    getLocation();
-    loadMarkersFromFirestore();
-  }
-
-  Future<void> getLocation() async {
-    try {
-      var location = Location();
-      var currentLocation = await location.getLocation();
-      setState(() {
-        this.currentLocation = currentLocation;
-      });
-    } catch (e) {
-      print('Error getting location: $e');
-    }
-  }
-
-  Future<void> _addCrimeMarker(
-    
-      LatLng position,
-      String description,
-      Uint8List imageBytes,
-      String reporterName,
-      bool isAnonymous,
-      String crimeType,
-      DateTime reportTime) async {
-    final markerId = MarkerId('${position.latitude}-${position.longitude}');
-    final marker = Marker(
-      markerId: markerId,
-      position: position,
-      icon: BitmapDescriptor.fromBytes(imageBytes),
-      infoWindow: InfoWindow(
-        title: 'Crime',
-        snippet: '$reporterName - $reportTime\n$Type: $crimeType\n$description',
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition:
+            CameraPosition(target: _initialPosition, zoom: 13),
+        onTap: _onMapTap,
+        markers: _buildMarkers(),
+        polylines: _polylines,
       ),
     );
+  }
 
+  Set<Marker> _buildMarkers() {
+    Set<Marker> markers = {};
+    if (_startLocation != null) {
+      markers.add(Marker(markerId: MarkerId('start'), position: _startLocation!));
+    }
+    if (_destinationLocation != null) {
+      markers.add(Marker(markerId: MarkerId('destination'), position: _destinationLocation!));
+    }
+    return markers;
+  }
+
+  void _onMapTap(LatLng tappedPoint) {
     setState(() {
-      crimeMarkers.add(marker);
-    });
-
-    DocumentReference<Map<String, dynamic>> docRef =
-        await FirebaseFirestore.instance.collection('crimeMarkers').add({
-      'latitude': position.latitude,
-      'longitude': position.longitude,
-      'description': description,
-      'imageUrl': '',
-      'reporterName': reporterName,
-      'isAnonymous': isAnonymous,
-      'reportTime': Timestamp.fromDate(reportTime),
-      'crimeType': crimeType,
-    });
-
-    String imageUrl = await _uploadImageToFirestore(imageBytes, docRef.id);
-
-    await docRef.update({'imageUrl': imageUrl});
-  }
-
-  Future<String> _uploadImageToFirestore(
-      Uint8List imageBytes, String documentId) async {
-    String imageName = DateTime.now().toIso8601String();
-
-    Reference ref =
-        FirebaseStorage.instance.ref().child('crime_images/$imageName.jpg');
-    UploadTask uploadTask = ref.putData(imageBytes);
-    TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
-
-    String imageUrl = await taskSnapshot.ref.getDownloadURL();
-
-    return imageUrl;
-  }
-
-  Future<void> loadMarkersFromFirestore() async {
-    final markers =
-        await FirebaseFirestore.instance.collection('crimeMarkers').get();
-
-    for (var doc in markers.docs) {
-      
-      final latitude = doc['latitude'] as double;
-      final longitude = doc['longitude'] as double;
-      final description = doc['description'] as String;
-      final imageUrl = doc['imageUrl'] as String;
-      final reporterName = doc['reporterName'] as String;
-      final isAnonymous = doc['isAnonymous'] as bool;
-      final reportTime = (doc['reportTime'] as Timestamp).toDate();
-      final crimeType = doc['crimeType'] as String;
-
-      final markerId = MarkerId('$latitude-$longitude');
-      final marker = Marker(
-        markerId: markerId,
-        position: LatLng(latitude, longitude),
-        icon: BitmapDescriptor.defaultMarkerWithHue(_getMarkerColor(crimeType)),
-        infoWindow: InfoWindow(
-          title: 'Crime',
-          snippet: '''
-          Crime Type: $crimeType
-          Description: $description
-          Reporter: $reporterName
-          Time: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(reportTime)}
-        ''',
-        ),
-      );
-
-      setState(() {
-        crimeMarkers.add(marker);
-      });
-    }
-  }
-
-  double _getMarkerColor(String crimeType) {
-    
-    switch (crimeType) {
-      case 'CarAccident':
-        return BitmapDescriptor.hueBlue;
-      case 'fireAccident':
-        return BitmapDescriptor.hueOrange;
-      case 'robberyAssault':
-        return BitmapDescriptor.hueRed;
-      default:
-        return BitmapDescriptor.hueRed;
-    }
-  }
-
-  Future<void> _showPlacePinDialog(LatLng position) async {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CrimeDetailsPage(position),
-      ),
-    ).then((crimeDetails) {
-      if (crimeDetails != null) {
-        _addCrimeMarker(
-          position,
-          crimeDetails['description'],
-          crimeDetails['imageBytes'],
-          crimeDetails['reporterName'],
-          crimeDetails['isAnonymous'],
-          crimeDetails['crimeType'],
-          crimeDetails['reportTime'],
-        );
+      if (_startLocation == null) {
+        _startLocation = tappedPoint;
+      } else if (_destinationLocation == null) {
+        _destinationLocation = tappedPoint;
+        _drawRoute();
+      } else {
+        _startLocation = tappedPoint;
+        _destinationLocation = null;
+        _polylines.clear();
       }
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Map'),
-        centerTitle: true,
-        backgroundColor: Colors.red,
-      ),
-      body: Stack(
-        children: [
-          if (currentLocation != null)
-            GoogleMap(
-              onMapCreated: (controller) {
-                setState(() {
-                  mapController = controller;
-                });
-              },
-              initialCameraPosition: CameraPosition(
-                target: LatLng(
-                    currentLocation!.latitude!, currentLocation!.longitude!),
-                zoom: 15.0,
-              ),
-              myLocationEnabled: true,
-              markers: crimeMarkers,
-              onTap: (LatLng position) {
-                if (placingPin) {
-                  _showPlacePinDialog(position);
-                }
-              },
-            ),
-          Positioned(
-            bottom: 16,
-            left: 16,
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  placingPin = !placingPin;
-                });
-              },
-              child: Text(placingPin ? 'Cancel Placing Pin' : 'Place Pin'),
-            ),
-          ),
-        ],
-      ),
-    );
+  Future<void> _drawRoute() async {
+    if (_startLocation == null || _destinationLocation == null) return;
+
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/directions/json?origin=${_startLocation!.latitude},${_startLocation!.longitude}&destination=${_destinationLocation!.latitude},${_destinationLocation!.longitude}&key=${Secrets.API_KEY}&alternatives=true');
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      Map data = jsonDecode(response.body);
+      List routes = data['routes'];
+      _polylines.clear();
+      bool anyRouteAdded = false;
+      List<LatLng>? fallbackRoute = null;
+
+      for (var i = 0; i < routes.length; i++) {
+        List<LatLng> polylineCoordinates = _decodePoly(routes[i]['overview_polyline']['points']);
+        bool containsAvoidCoordinate = polylineCoordinates.any((point) => _isNearAvoid(point));
+
+        if (!containsAvoidCoordinate) {
+          _polylines.add(_createPolyline(i, polylineCoordinates));
+          anyRouteAdded = true;
+        } else  { 
+          if(fallbackRoute == null)
+          fallbackRoute = polylineCoordinates;
+        }
+      }
+
+      if (!anyRouteAdded && fallbackRoute != null) {
+        _askUserForFallback(fallbackRoute); // Ask user to accept the fallback route
+      } else {
+        setState(() {});
+      }
+    } else {
+      throw Exception('Failed to load directions');
+    }
   }
-}
 
-class CrimeDetailsPage extends StatefulWidget {
-  
-  final LatLng position;
-
-  CrimeDetailsPage(this.position);
-
-  @override
-  _CrimeDetailsPageState createState() => _CrimeDetailsPageState();
-}
-
-class _CrimeDetailsPageState extends State<CrimeDetailsPage> {
-  
-  String description = '';
-  Uint8List imageBytes = Uint8List(0);
-  String reporterName = '';
-  bool isAnonymous = false;
-  String crimeType = 'Car Accident';
-  DateTime reportTime = DateTime.now();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Crime Details'),
-        backgroundColor: Colors.red,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DropdownButton<String>(
-              value: crimeType,
-              onChanged: (value) {
-                setState(() {
-                  crimeType = value!;
-                });
-              },
-              items: <String>[
-                'Car Accident',
-                'Fire',
-                'Robbery/Assault',
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            TextField(
-              onChanged: (value) => description = value,
-              decoration: InputDecoration(labelText: 'Description'),
-            ),
-            TextField(
-              onChanged: (value) => reporterName = value,
-              decoration: InputDecoration(labelText: 'Reporter Name'),
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: isAnonymous,
-                  onChanged: (value) {
-                    setState(() {
-                      isAnonymous = value!;
-                    });
-                  },
-                ),
-                Text('Anonymous'),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final imagePicker = ImagePicker();
-                final pickedFile =
-                    await imagePicker.pickImage(source: ImageSource.gallery);
-
-                if (pickedFile != null) {
-                  final bytes = await pickedFile.readAsBytes();
-                  setState(() {
-                    imageBytes = bytes;
-                  });
-                }
-              },
-              child: Text('Pick Image'),
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.all(20),
-                primary: Colors.redAccent,
-                onPrimary: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
+  void _askUserForFallback(List<LatLng> fallbackRoute) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Routing Issue'),
+          content: const Text('All possible routes pass through dangerous areas. Display the best available route?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('No'),
               onPressed: () {
-                Navigator.pop(
-                  context,
-                  {
-                    'description': description,
-                    'imageBytes': imageBytes,
-                    'reporterName': reporterName,
-                    'isAnonymous': isAnonymous,
-                    'crimeType': crimeType,
-                    'reportTime': reportTime,
-                  },
-                );
+                Navigator.of(context).pop();
               },
-              
-              child: Text('Submit'),
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.all(20),
-                primary: Colors.redAccent,
-                onPrimary: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-              ),
+            ),
+            TextButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _polylines.add(_createPolyline(0, fallbackRoute)); // Add fallback route
+                setState(() {});
+              },
             ),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
+
+  Polyline _createPolyline(int index, List<LatLng> coordinates) {
+    return Polyline(
+      polylineId: PolylineId('route$index'),
+      color: _getRouteColor(index),
+      points: coordinates,
+      width: 5,
+    );
+  }
+
+  Color _getRouteColor(int routeIndex) {
+    switch (routeIndex % 3) {
+      case 0:
+        return Colors.blue;
+      case 1:
+        return Colors.green;
+      case 2:
+        return Colors.black;
+      default:
+        return Colors.blue;
+    }
+  }
+
+  bool _isNearAvoid(LatLng point) {
+    return _avoidCoordinates.any((avoid) => _calculateDistance(point, avoid) < 1000);
+  }
+
+  List<LatLng> _decodePoly(String poly) {
+    List<int> list = poly.codeUnits;
+    List<double> latLngList = [];
+    int index = 0;
+    int current = 0;
+    int bit = 0;
+    int result = 0;
+    int shift;
+
+    while (index < poly.length) {
+      current = list[index] - 63;
+      result |= (current & 0x1f) << (5 * bit);
+      if (current < 0x20) {
+        shift = ((result & 1) == 1 ? ~(result >> 1) : (result >> 1));
+        latLngList.add(shift.toDouble());
+        index++;
+        bit = 0;
+        result = 0;
+        continue;
+      }
+      index++;
+      bit++;
+    }
+
+    List<LatLng> polylineCoordinates = [];
+    double lat = 0;
+    double lng = 0;
+
+    for (int i = 0; i < latLngList.length; i += 2) {
+      lat += latLngList[i] / 100000.0;
+      lng += latLngList[i + 1] / 100000.0;
+      polylineCoordinates.add(LatLng(lat, lng));
+    }
+
+    return polylineCoordinates;
+  }
+
+  double _calculateDistance(LatLng point1, LatLng point2) {
+    final lat1 = point1.latitude;
+    final lon1 = point1.longitude;
+    final lat2 = point2.latitude;
+    final lon2 = point2.longitude;
+    const R = 6371000.0; // Earth radius in meters
+    final phi1 = lat1 * (pi / 180);
+    final phi2 = lat2 * (pi / 180);
+    final deltaPhi = (lat2 - lat1) * (pi / 180);
+    final deltaLambda = (lon2 - lon1) * (pi / 180);
+
+    final a = (sin(deltaPhi / 2) * sin(deltaPhi / 2)) +
+              (cos(phi1) * cos(phi2) * sin(deltaLambda / 2) * sin(deltaLambda / 2));
+    final c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+    return R * c; // Distance in meters
+  }
 }
+
